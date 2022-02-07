f5ae55314c348301910590ee1534fa4afefcb1fb
vijay010498 <vijayaraghavan16315@cse.ssn.edu.in>
2022-02-07 16:36:01 +0530

    gg

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -1,8 +1,19 @@
 const calcTip = (total, tipPercent) => {
-    const tip = total * tipPercent
-    return total + tip
-}
+  const tip = total * tipPercent;
+  return total + tip;
+};
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject("Numbers must be non-negative");
+      }
 
+      resolve(a + b);
+    }, 2000);
+  });
+};
 module.exports = {
-    calcTip
-}
+  calcTip,
+  add,
+};
--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,9 +1,9 @@
-const {calcTip} = require("../src/math")
+const { calcTip, add } = require("../src/math");
 
-test('should calculate total tip',() =>{
-    const total=  calcTip(10,.3);
-    expect(total).toBe(13);
-})
+test("should calculate total tip", () => {
+  const total = calcTip(10, 0.3);
+  expect(total).toBe(13);
+});
 
 //
 // Why test?
@@ -15,3 +15,25 @@ test('should calculate total tip',() =>{
 //   - Collaborating
 //   - Profiling
 // - Peace of mind
+
+// async 1 way
+// test("Async test demo", (done) => {
+//   setTimeout(() => {
+//     expect(1).toBe(1);
+//     done();
+//   }, 2000);
+// });
+
+// async 2 way
+test("Async promise based should add two numbers", (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+// async 3 days
+test("Async/ await ", async () => {
+  const sum = await add(2, 3);
+  expect(sum).toBe(5);
+});
